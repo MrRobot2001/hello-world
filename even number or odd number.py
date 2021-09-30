e2c2b12201492fa627c1c1cfbad7c454250c1882
dilip singhal <72966696+Dilipsinghal66@users.noreply.github.com>
2021-10-01 01:55:35 +0530

    Update even number or odd number.py

--- even number or odd number.py
+++ even number or odd number.py
@@ -1,7 +1,7 @@
 n=int(input("Enter a number: "))
 
 if(n%2==0):
-    print("The value entered",n,"is even number")
+    print("The value ",n,"is even number")
 
 else:
-    print("The value entered",n,"is Odd number")
+    print("The value ",n,"is Odd number")
